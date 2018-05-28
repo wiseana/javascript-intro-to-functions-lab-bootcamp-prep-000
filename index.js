f1ad6b70de361842440c9e651514fffc863cf3f9
Anastasia Wise <wise.anastasia@gmail.com>
2018-05-28 14:49:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,7 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (`${string}`.toUpperCase()===`${string}`) {
-    return 'YES INDEED!'}}
+    return 'YES INDEED!'}
+    else if (`${string}`.toLowerCase()===`${string}`) {
+      return `I can't hear you!`}
+      else if (`${string}`===`I love you, Grandma.`) {
