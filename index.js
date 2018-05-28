0003485251d8a6ab711b150b71e1b24127e73b42
Anastasia Wise <wise.anastasia@gmail.com>
2018-05-28 14:53:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,5 @@ function sayHiToGrandma(string) {
     else if (`${string}`.toLowerCase()===`${string}`) {
       return `I can't hear you!`}
       else if (`${string}`===`I love you, Grandma.`) {
+        return 'I love you, too.'
+      }}
